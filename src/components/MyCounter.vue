d1ed0d1618b1dce2bb29e81f648c9c2619d852e8
Jennifer Romero <romerojennifer722@gmail.com>
2024-05-20 16:20:34 -0500

    counter Tailwind CSS

---
+++ src/components/MyCounter.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+    <h3>Counter {{ counter }}</h3>
+    <h3>Square {{ squareCounter }}</h3>
+
+    <div>
+      <button class="btn" @click="counter++">+1</button>
+      <button class="btn" @click="counter--">-1</button>
+    </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+import { ref, computed } from 'vue';
+
+//En JS<
+// const props = defineProps({
+//   value: { type: Number, required: true },
+// });
+
+interface Props {
+  value: number;
+}
+
+const props = defineProps<Props>();
+
+const counter = ref(props.value);
+const squareCounter = computed(() => counter.value * counter.value);
+</script>
+
+<style scoped>
+.btn {
+  @apply p-5 bg-blue-500 rounded hover:bg-blue-700 mr-2;
+}
+</style>

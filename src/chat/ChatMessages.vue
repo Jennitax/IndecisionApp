c00c8f75042ace11381d975b85429e0f15aa7728
Jennifer Romero <romerojennifer722@gmail.com>
2024-05-20 20:29:03 -0500

    itsMine

---
+++ src/chat/ChatMessages.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import ChatBubble from './ChatBubble.vue';
+
+interface Props {
+  messages: ChatMessage[];
+}
+defineProps<Props>();
+</script>
+
+<template>
+  <div class="flex-1 overflow-y-auto p-4">
+    <div class="flex flex-col space-y-2">
+      <!-- Messages go here -->
+
+      <ChatBubble v-for="message in messages" :key="message.id" v-bind="message" />
+      <!-- :its-mine="message.itsMine"
+        :message="message.message"
+        :image="message.image" -->
+    </div>
+  </div>
+</template>
---
+++ src/views/indecisionView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="bg-gray-100 h-screen flex flex-col max-w-lg mx-auto">
+    <div class="bg-blue-500 p-4 text-white flex justify-between items-center">
+      <span>Mi esposa</span>
+    </div>
+
+    <!--- Chat Messages -->
+    <ChatMessages :messages="messages" />
+
+    <!--- MessageBox -->
+    <MessageBox @send-message="onMessage" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import ChatMessages from '@/chat/ChatMessages.vue';
+import MessageBox from '@/chat/MessageBox.vue';
+import { ref } from 'vue';
+
+const messages = ref<ChatMessage[]>([
+  {
+    id: new Date().getTime(),
+    message: 'Hola mundo',
+    itsMine: true,
+  },
+  {
+    id: new Date().getTime() + 1,
+    message: 'No',
+    itsMine: false,
+    image: 'https://yesno.wtf/assets/no/9-dc99c0e3c066b28d3a12262692cd5432.gif',
+  },
+]);
+
+const onMessage = (text: string) => {
+  messages.value.push({
+    id: new Date().getTime(),
+    itsMine: true,
+    message: text,
+  });
+};
+</script>

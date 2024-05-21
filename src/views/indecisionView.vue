9a4953fd551fbe4d8d12ed376773632bb63964be
Jennifer Romero <romerojennifer722@gmail.com>
2024-05-20 20:50:10 -0500

    Response

--- src/views/indecisionView.vue
+++ src/views/indecisionView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-gray-100 h-screen flex flex-col max-w-lg mx-auto">
     <div class="bg-blue-500 p-4 text-white flex justify-between items-center">
-      <span>Mi esposa</span>
+      <span>My Husband</span>
     </div>
 
     <!--- Chat Messages -->
@@ -15,27 +15,7 @@
 <script setup lang="ts">
 import ChatMessages from '@/chat/ChatMessages.vue';
 import MessageBox from '@/chat/MessageBox.vue';
-import { ref } from 'vue';
+import { useChat } from '@/composables/useChat';
 
-const messages = ref<ChatMessage[]>([
-  {
-    id: new Date().getTime(),
-    message: 'Hola mundo',
-    itsMine: true,
-  },
-  {
-    id: new Date().getTime() + 1,
-    message: 'No',
-    itsMine: false,
-    image: 'https://yesno.wtf/assets/no/9-dc99c0e3c066b28d3a12262692cd5432.gif',
-  },
-]);
-
-const onMessage = (text: string) => {
-  messages.value.push({
-    id: new Date().getTime(),
-    itsMine: true,
-    message: text,
-  });
-};
+const { messages, onMessage } = useChat();
 </script>
